011982dbe8fe36dd3dd5d84433db6bdac9813de4
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-10-29 20:44:27 -0600

    Test show & index, rubocop offenses

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class DriversController < ApplicationController
   before_action :set_driver, only: [:show]
 
--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -1,7 +1,21 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class DriversControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @driver_ok = drivers(:driver_ok)
+  end
+
+  test 'visit the index controller path' do
+    get drivers_path
+
+    assert_response :success
+  end
+
+  test 'visit the show controller path' do
+    get driver_path(@driver_ok.id)
+
+    assert_response :success
+  end
 end
