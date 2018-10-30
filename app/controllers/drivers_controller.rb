2d432de2fd997f05709df0af3cb6d82fc70298f0
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-10-29 20:09:21 -0600

    boostrap cdn, show and index views with haml

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,15 @@
+class DriversController < ApplicationController
+  before_action :set_driver, only: [:show]
+
+  def index
+    @drivers = Driver.all
+  end
+
+  def show; end
+
+  private
+
+  def set_driver
+    @driver = Driver.find(params[:id])
+  end
+end
