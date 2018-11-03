e87ab70e5c44d642342a15947e96cc468d17d74d
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-11-03 17:47:21 -0600

    links to sign_in, sign_up and sign_out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,21 @@
 
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
     @users = User.all
+  end
+
+  def show; end
+  
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :business_name)
   end
 end
