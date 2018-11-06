3b3810d773938176ec7b09260359d9b813cefd0d
Benjamin Peña Olvera <benjamin.pena.olvera@gmail.com>
2018-11-06 09:54:03 -0600

    Trying to validate some actions in update ethod controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # validates :name, presence: true, allow_blank: false, on: :update, message: 'algo'
+  validates_presence_of :name, on: :update, message: 'algo'
+  validates :business_name,  presence: {message: "your custom message"}, on: :update
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
